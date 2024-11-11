cf90a0782209a3fff3273255dc98b55829ce4078
tangbolll <tangboll5075@gmail.com>
2024-11-12 01:09:22 +0900

    DeleteButton, WriteComments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import MakeRoutine from "./components/makeRoutine";
 import TimeSelector from "./components/TimeSelector";
 import WriteNickname from "./components/WriteNickname";
 import CompleteButton from "./components/CompleteButton";
+import DeleteButton from "./components/DeleteButton";
+import WriteComments from "./components/WriteComments";
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
       <WriteNickname />
       <TimeSelector />
       <CompleteButton />
+      <WriteComments/>
+      <DeleteButton/>
     </div>
   );
 }
