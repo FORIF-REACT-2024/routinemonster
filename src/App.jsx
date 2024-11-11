c096e15424f60dc146b5dd7ec6e55d248fc0aa0f
tangbolll <tangboll5075@gmail.com>
2024-11-11 23:45:23 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+<<<<<<< Updated upstream
 import GoogleLogin from "./components/googleLogin";
 import MakeRoutine from "./components/makeRoutine";
 import TimeSelector from "./components/TimeSelector";
 import WriteNickname from "./components/WriteNickname";
 import CompleteButton from "./components/CompleteButton";
+=======
+>>>>>>> Stashed changes
 import DeleteButton from "./components/DeleteButton";
 
 function App() {
 
   return (
     <div>
+<<<<<<< Updated upstream
       <GoogleLogin />
       <MakeRoutine />
       <WriteNickname />
       <TimeSelector />
       <CompleteButton />
+=======
+>>>>>>> Stashed changes
       <DeleteButton />
     </div>
   );
