02ea2b32f16fab27a4bb6f86beaf427481cf2ba8
tangbolll <tangboll5075@gmail.com>
2024-11-11 23:23:32 +0900

    Create DeleteButton.jsx

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,9 @@
+export default function DeleteButton() {
+    return (
+        <div>
+            <button className="border border-red-200 text-black font-medium w-10 h-7 rounded-lg">
+                삭제
+            </button>
+        </div>
+    );
+}
