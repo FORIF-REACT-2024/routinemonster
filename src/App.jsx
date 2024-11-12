a04dab2fdb4639f33a075ec8e7f5518904a0ccba
Junill Jang <jang020407@naver.com>
2024-11-12 18:52:36 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-<<<<<<< Updated upstream
-import GoogleLogin from "./components/googleLogin";
-import MakeRoutine from "./components/makeRoutine";
-import TimeSelector from "./components/TimeSelector";
-import WriteNickname from "./components/WriteNickname";
-import CompleteButton from "./components/CompleteButton";
-=======
->>>>>>> Stashed changes
 import DeleteButton from "./components/DeleteButton";
 
 function App() {
 
   return (
     <div>
-<<<<<<< Updated upstream
-      <GoogleLogin />
-      <MakeRoutine />
-      <WriteNickname />
-      <TimeSelector />
-      <CompleteButton />
-=======
->>>>>>> Stashed changes
       <DeleteButton />
     </div>
   );
