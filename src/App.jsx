bb7f019aeacaee26cd68734e3144a812866b79af
tangbolll <your-github-noreply-email@users.noreply.github.com>
2024-11-18 13:47:09 +0900

    TY/ComponentModify

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import CategoryButton from "./components/CategoryBox";
+import DeleteButton from "./components/DeleteButton";
+import CommentInput from "./components/CommentInput";
 
 function App() {
 
   return (
     <div>
       <CategoryButton category={3} />
+      <DeleteButton/>
+      <CommentInput/>
     </div>
   );
 }
---
+++ src/components/CommentInput.jsx
@@ -0,0 +1,25 @@
+const CommentInput = () => {
+    return (
+      <form>
+      <div className="max-w-3xl">  {/* 최대 너비 제한 및 가운데 정렬 */}
+        <div className="flex items-center bg-blue-50 rounded-3xl p-4">  {/* 더 둥근 테두리 */}
+          <label className="flex items-center w-full">
+            오늘의 코멘트
+          
+          <input 
+            id="commentInput"
+            type="text"
+            placeholder="나는 오늘도 눈물을 흘린다...@@"
+            className="flex-1 bg-white rounded-2xl p-2 outline-none text-gray-700 ml-4" 
+          />
+          </label>
+        </div>
+      </div>
+
+
+    </form>
+    );
+  };
+  
+  export default CommentInput
+;
