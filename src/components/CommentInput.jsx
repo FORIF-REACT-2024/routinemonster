905993900b70449b9760013469cf506461f7441f
tangbolll <tangboll5075@gmail.com>
2024-11-26 17:56:14 +0900

    Update CommentInput.jsx

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,25 +1,23 @@
-const CommentInput = () => {
-    return (
+const CommentInput = ({ value, onChange }) => {
+  return (
       <form>
-      <div className>
-        <div className="flex items-center bg-blue-50 rounded-3xl p-4">  {/* 더 둥근 테두리 */}
-          <label className="flex items-center w-full">
-            오늘의 코멘트
-          
-          <input 
-            id="commentInput"
-            type="text"
-            placeholder="나는 오늘도 눈물을 흘린다...@@"
-            className="flex-1 bg-white rounded-2xl p-2 outline-none text-gray-700 ml-4" 
-          />
-          </label>
-        </div>
-      </div>
+          <div>
+              <div className="flex items-center bg-blue-50 rounded-3xl p-4">
+                  <label className="flex items-center w-full">
+                      오늘의 코멘트
+                      <input 
+                          id="commentInput"
+                          type="text"
+                          value={value}
+                          onChange={(e) => onChange(e.target.value)}
+                          placeholder="나는 오늘도 눈물을 흘린다...@@"
+                          className="flex-1 bg-white rounded-2xl p-2 outline-none text-gray-700 ml-4" 
+                      />
+                  </label>
+              </div>
+          </div>
+      </form>
+  );
+};
 
-
-    </form>
-    );
-  };
-  
-  export default CommentInput
-;
+export default CommentInput;
