ab6d987410e28dc19b524eb777d34d358ac9c53f
Junill Jang <jang020407@naver.com>
2024-12-21 18:14:17 +0900

    캘린더 1월 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -29,7 +29,7 @@ const Main = () => {
 
   return (
     <div className={`flex flex-col items-center justify-center min-h-screen ${darkMode ? 'bg-gray-900 text-white' : 'bg-blue-100 text-black'}`}>
-      <div className="flex items-center justify-center w-full pb-6">
+      <div className="flex items-center justify-center w-full pb-6 mt-10">
         <p className="text-5xl">Routine Monster</p>
         <button
           onClick={toggleDarkMode}
@@ -41,7 +41,7 @@ const Main = () => {
 
       <div className="flex">
         <div className="pr-3">
-          <Profile setData={setPage} darkMode={darkMode} userInfo={userInfo} />
+          <Profile setData={setPage} darkMode={darkMode} />
         </div>
 
         <div className={`flex flex-col items-center w-[750px] border-2 ${darkMode ? 'border-blue-400 bg-gray-800' : 'border-blue-400 bg-white'} rounded-2xl`}>
